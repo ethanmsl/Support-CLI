6392cc5153f5ccc3afeb2ac7ceb1729e94e2f432
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-09-14 12:15:18 -0700

    improved robustness of code and improved comments and presentation

--- zsh/sources/cursor-reflect-vimode.zsh
+++ zsh/sources/cursor-reflect-vimode.zsh
@@ -1,5 +1,10 @@
 #!/bin/zsh
 
+
+# ########################### --ViMode Cursor Shape-- ####################### #
+
+
+# ----------- Cursor-Change with Mode-Change -----------
 # Change cursor shape for different vi modes.
 # 'echo -ne' : don't print newline at end and read '\'
 function zle-keymap-select {
@@ -20,25 +25,34 @@ function zle-keymap-select {
 zle -N zle-keymap-select
 # ^create user-defined widget ('zle -N <func>')
 
+
+# ----------- Cursor+Mode Change with NewLine -----------
+# this script starts us in <insert> mode AND sets cursor to blink-beam
+# and fires on every new line
+zle-line-init() {
+    zle -K viins
+    # ^select keymap ('zle -K <selected_keymap>'); starts us in <insert> mode
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+# ^create user-defined widget ('zle -N <func>')
+
+
+# ----------- Cursor-Change with Terminal -----------
 # Use beam shape cursor on startup
 echo -ne '\e[5 q'
 
-# 'preexec()': executed right before entered command is
-#  so it should effectively set the cursor shape for the next line
-# (useful when a command is entered in <normal> mode,
-#  to prevent inheriting the block-cursor shape)
-preexec() { echo -ne '\e[5 q' ;}
-
 
 ########### back-pocket function ###########
-# # this script starts us in <insert> mode AND sets cursor to blink-bar
-# # the first part is redundant with standard behavior
-# # the second is redundant with the "use beam shape cursor on startup" line
-# # I'm removing this, but may be useful to have in future
-# zle-line-init() {
-#     zle -K viins
-#     # ^select keymap ('zle -K <selected_keymap>'); starts us in <insert> mode
-#     echo -ne "\e[5 q"
-# }
-# zle -N zle-line-init
-# # ^create user-defined widget ('zle -N <func>')
+# INFO: as far as I can tell this behavior is reundant, 
+#       but weaker, than zle-line-init()'s
+#       Commenting out, but leaving for possible future use.
+#
+# # ----------- Cursor-Change before Command-Processing -----------
+# # 'preexec()': executed right before entered command is
+# #  so it should effectively set the cursor shape for the next line
+# # (useful when a command is entered in <normal> mode,
+# #  to prevent inheriting the block-cursor shape)
+# #  NOTE: does NOT fire if no command (neither "good" nor "bad") is entered
+# #        so '[enter]' on a blank line in <normal> mode will not fire preexec()
+# preexec() { echo -ne '\e[5 q' ;}
